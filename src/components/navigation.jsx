3edf150352fa9b39621368a02a05f622567c0493
Matias Durand <mati.durand98@gmail.com>
2025-05-17 19:26:43 -0300

    Botón número agregado para mobile

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -13,83 +13,95 @@ export const Navigation = () => {
   };
 
   return (
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
+    <>
+      <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1"
+            >
+              {" "}
+              <span className="sr-only">Toggle navigation</span>{" "}
+              <span className="icon-bar"></span>{" "}
+              <span className="icon-bar"></span>{" "}
+              <span className="icon-bar"></span>{" "}
+            </button>
+            <a
+              className="navbar-brand page-scroll"
+              href="#page-top"
+              onClick={(e) => {
+                e.preventDefault();
+                handleScrollTo("page-top");
+              }}
+            >
+              <img src="img/logoSegumar.png" className="logoSegumar" alt="Segumar Logo" />
+            </a>
+
+          </div>
+
+          <div
+            className="collapse navbar-collapse"
+            id="bs-example-navbar-collapse-1"
           >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-          </button>
-          <a
-            className="navbar-brand page-scroll"
-            href="#page-top"
-            onClick={(e) => {
-              e.preventDefault();
-              handleScrollTo("page-top");
-            }}
-          >
-            <img src="img/logoSegumar.png" className="logoSegumar" alt="Segumar Logo" />
-          </a>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("features"); }}>
+                Quienes somos
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("about"); }}>
+                Seguridad Física
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("services"); }}>
+                Seguridad Electrónica
+              </a>
+            </li>
+        {/*   <li>
+              <a href="#portfolio" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("portfolio"); }}>
+                Gallery
+              </a>
+            </li>*/}
+            <li>
+              <a href="#testimonials" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("testimonials"); }}>
+                Nuestros Clientes
+              </a>
+            </li>
+          {/*  <li>
+              <a href="#team" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("team"); }}>
+                Team
+              </a>
+            </li>*/}
+          {/* <li>
+              <Link className="page-scroll" to="/page2">
+                Page2
+              </Link>
+            </li>*/}
+            <li>
+              <a href="#contact" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("contact"); }}>
+                Contacto
+              </a>
+            </li>
+          </ul>
 
+          </div>
         </div>
-
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-         <ul className="nav navbar-nav navbar-right">
-          <li>
-            <a href="#features" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("features"); }}>
-              Quienes somos
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("about"); }}>
-              Seguridad Física
-            </a>
-          </li>
-          <li>
-            <a href="#services" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("services"); }}>
-              Seguridad Electrónica
-            </a>
-          </li>
-       {/*   <li>
-            <a href="#portfolio" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("portfolio"); }}>
-              Gallery
-            </a>
-          </li>*/}
-          <li>
-            <a href="#testimonials" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("testimonials"); }}>
-              Nuestros Clientes
-            </a>
-          </li>
-        {/*  <li>
-            <a href="#team" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("team"); }}>
-              Team
-            </a>
-          </li>*/}
-         {/* <li>
-            <Link className="page-scroll" to="/page2">
-              Page2
-            </Link>
-          </li>*/}
-          <li>
-            <a href="#contact" className="page-scroll" onClick={(e) => { e.preventDefault(); handleScrollTo("contact"); }}>
-              Contacto
-            </a>
-          </li>
-        </ul>
-
-        </div>
-      </div>
-    </nav>
+      </nav>
+     <div className="whatsapp-bar">
+    <a
+      href="https://wa.me/5491123456789"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="whatsapp-button2"
+    >
+      📞 11 2345 6789
+    </a>
+  </div>
+  </>
   );
 };
