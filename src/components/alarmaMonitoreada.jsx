22b5f60f3892a7517236d7cc85e338b8c6a3577e
Matias Durand <mati.durand98@gmail.com>
2025-05-28 19:45:22 -0300

    Ajustando detalles

--- src/components/alarmaMonitoreada.jsx
+++ src/components/alarmaMonitoreada.jsx
@@ -15,27 +15,32 @@ export const AlarmaMonitoreada = (props) => {
           </p>
         </div>
       */}
-        <div className="seguridadFisicaContainer">
-          <div className="seguridadFisicaCuadro">
-            <img src="img/cam.png" className="iconoServicios" />
-            <h4>VIVÍ PROTEGIDO LAS 24HS</h4>
-            <p>Nunca más ingresaras solo a tu casa. Desde SEGUMAR estaremos cuidándote las 24 horas mediante las cámaras enlazadas a la central.</p>
-          
+      <div className="AlarmaMonitoreadaContainer">
+          <div className="AlarmaMonitoreadaCuadro glass" data-aos="zoom-in">
+            <div className="icon-wrapper">
+              <img src="img/cam.png" className="iconoServiciosAM" />
+            </div>
+            <h4 className="AlarmaMonitoreadaTitulo">VIVÍ PROTEGIDO LAS 24HS</h4>
+            <p>Nunca más ingresarás solo a tu casa. Desde SEGUMAR estaremos cuidándote las 24 horas mediante las cámaras enlazadas a la central.</p>
           </div>
-          <div className="seguridadFisicaCuadro">
-            <img src="img/van.png" className="iconoServicios" />
-              <h4>MOVIL CON RESPUESTA INMEDIATA</h4>
-              <p>El servicio de seguridad remota 2.0 es significativamente más barato que la seguridad física pudiendo lograr de este modo abaratar el costo de expensas.</p>
-              
+
+          <div className="AlarmaMonitoreadaCuadro glass">
+            <div className="icon-wrapper">
+              <img src="img/van.png" className="iconoServiciosAM" />
+            </div>
+            <h4 className="AlarmaMonitoreadaTitulo">MÓVIL CON RESPUESTA INMEDIATA</h4>
+            <p>El servicio de seguridad remota 2.0 es más barato que la seguridad física, ayudando a reducir costos de expensas.</p>
           </div>
-          <div className="seguridadFisicaCuadro">
-            <img src="img/ahorrar.png" className="iconoServicios" />
-              <h4>AHORRÁ LA COMPRA DEL EQUIPO CON NUESTROS KIT EN COMODATO</h4>
-              <p>En Segumar contamos con kits de alarmas en modalidad COMODATO, para que protejas tu hogar sin necesidad de realizar una gran inversión inicial. Sólo abonás el costo de instalación y empezás a disfrutar de la tranquilidad que necesitás. Nuestros kits incluyen sensores y comunicadores GPRS.</p>
-            
+
+          <div className="AlarmaMonitoreadaCuadro glass large">
+            <div className="icon-wrapper">
+              <img src="img/ahorrar.png" className="iconoServiciosAM" />
+            </div>
+            <h4 className="AlarmaMonitoreadaTitulo">AHORRÁ CON NUESTROS KITS EN COMODATO</h4>
+            <p>Protegé tu hogar sin gran inversión inicial. Sólo abonás la instalación y disfrutás de tranquilidad con sensores y comunicadores GPRS.</p>
           </div>
-         
         </div>
+
         
       </div>
     </div>
