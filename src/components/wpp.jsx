a767bfd1cfc1878692429aab6635cdeb5eb9d275
Matias Durand <mati.durand98@gmail.com>
2025-06-26 17:20:16 -0300

    Correccion direcciones

--- src/components/wpp.jsx
+++ src/components/wpp.jsx
@@ -4,7 +4,7 @@ export const Wpp = () => {
   return (
     <div id="Wpp" className="wpp">
         <a
-            href="https://wa.me/2234554387"
+            href="https://wa.me/2236852201"
             target="_blank"
             rel="noopener noreferrer"
             className="whatsapp-button"
